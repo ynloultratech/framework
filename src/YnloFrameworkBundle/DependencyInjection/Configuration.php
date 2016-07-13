a6e27ddffc380e734b53679547b9d6126dc47c8e
Rafael SR <raf.rsr@gmail.com>
2016-07-13 19:20:22 -0400

    adding icon sets

--- src/YnloFrameworkBundle/DependencyInjection/Configuration.php
+++ src/YnloFrameworkBundle/DependencyInjection/Configuration.php
@@ -28,15 +28,21 @@ class Configuration implements ConfigurationInterface
         $rootNode->booleanNode('debug')->defaultValue("%kernel.debug%");
         $rootNode->booleanNode('pace')->defaultValue(true)->info('Enable or disable the Pace loader.');
         $rootNode->booleanNode('ajax_forms')->defaultValue(false)->info('Use ajax forms in bundles that support this.');
+        $rootNode->variableNode('icons')->defaultValue(['fontawesome'])->info('One or more than one icon library, available: fontawesome, glyphicons', 'icomoon');
 
-        $this->createAssetConfig($rootNode, [
-            'jquery' => 'bundles/ynloframework/js/vendor/jquery/jquery.min.js',
-            'jquery_form' => 'bundles/ynloframework/js/vendor/jquery.form/jquery.form.js',
-            'pace_js' => 'bundles/ynloframework/js/vendor/pace/pace.js',
-            'pace_css' => 'bundles/ynloframework/js/vendor/pace/pace.css',
-            'bootstrap_js' => 'bundles/ynloframework/js/vendor/bootstrap/js/bootstrap.min.js',
-            'bootstrap_css' => 'bundles/ynloframework/js/vendor/bootstrap/css/bootstrap.min.css',
-        ]);
+        $this->createAssetConfig(
+            $rootNode, [
+                'jquery' => 'bundles/ynloframework/js/vendor/jquery/jquery.min.js',
+                'jquery_form' => 'bundles/ynloframework/js/vendor/jquery.form/jquery.form.js',
+                'pace_js' => 'bundles/ynloframework/js/vendor/pace/pace.js',
+                'pace_css' => 'bundles/ynloframework/js/vendor/pace/pace.css',
+                'bootstrap_js' => 'bundles/ynloframework/js/vendor/bootstrap/js/bootstrap.min.js',
+                'bootstrap_css' => 'bundles/ynloframework/js/vendor/bootstrap/css/bootstrap.min.css',
+                'fontawesome' => 'bundles/ynloframework/js/vendor/font-awesome/css/font-awesome.min.css',
+                'glyphicons' => 'bundles/ynloframework/js/vendor/glyphicons/css/glyphicons.css',
+                'icomoon' => 'bundles/ynloframework/js/vendor/icomoon/icomoon.css',
+            ]
+        );
 
         return $treeBuilder;
     }
