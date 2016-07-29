e0af3d4189fa79df36449ce182cceaaa7462ba17
Rafael Santos <raf.rsr@gmail.com>
2016-07-29 15:02:15 -0500

    fix function name verification

--- src/YnloFrameworkBundle/functions.php
+++ src/YnloFrameworkBundle/functions.php
@@ -16,7 +16,7 @@ if (!function_exists('dump')) {
         }
     }
 }
-if (!function_exists('array_value_expected')) {
+if (!function_exists('array_key_value')) {
     /**
      * Extract some value from array using a key path,
      * return default value if the path don't exist
