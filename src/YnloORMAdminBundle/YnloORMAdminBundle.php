9cfa2ab4d98ce3094ff2a3d0ae5682028f649fe1
Rafael Santos <raf.rsr@gmail.com>
2016-08-03 19:32:08 -0500

    [Admin] drafting ORM admin bundle to override some parts of sonata

---
+++ src/YnloORMAdminBundle/YnloORMAdminBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the YNLOFramework package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace YnloFramework\YnloORMAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YnloORMAdminBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'SonataDoctrineORMAdminBundle';
+    }
+}
